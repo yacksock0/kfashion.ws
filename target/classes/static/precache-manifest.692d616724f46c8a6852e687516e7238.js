ec7c0b02a776912b8a0fef018af854eddbf70fcf
jang6845 <jangseongyeol@aetherit.io>
2020-06-12 18:12:15 +0900

    장성열 커밋

---
+++ target/classes/static/precache-manifest.692d616724f46c8a6852e687516e7238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c65d61a8bc091b676a0c87c7ea9bb72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19c9e4204ba7bc444bc9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b68300c64544642ecebb",
+    "url": "/static/js/2.aba6966e.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.aba6966e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c9e4204ba7bc444bc9",
+    "url": "/static/js/main.1fe48a2a.chunk.js"
+  },
+  {
+    "revision": "1e184bed41a810564677",
+    "url": "/static/js/runtime-main.1ca79223.js"
+  }
+]);
