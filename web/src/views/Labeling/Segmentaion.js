6296f064569ee59cd0047b72be8d7b83874e1b9d
ho <chsgh472@aetherit.io>
2020-06-16 18:21:07 +0900

    Front-end First

---
+++ web/src/views/Labeling/Segmentaion.js
@@ -0,0 +1,65 @@
+import React from "react";
+import {withSnackbar} from "notistack";
+import {withRouter} from "react-router-dom";
+import {withStyles} from "@material-ui/core/styles";
+
+import {Container, Toolbar, Typography, Button, CircularProgress} from "@material-ui/core";
+import * as store from "../../stores/AuthStore";
+
+
+const styles = theme => ({
+    mainContainer: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    mainContent: {
+        marginTop: theme.spacing(2),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    toolbar: {
+        width: '100%',
+    },
+});
+
+class Segmentation extends React.Component {
+    componentDidMount() {
+        this.props.enqueueSnackbar("Segmentation Work", {
+            variant: 'info'
+        });
+    }
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <Container component="main" className={classes.mainContainer}>
+                <div className={classes.appBarSpacer} />
+                <div className={classes.mainContent}>
+                    <Toolbar className={classes.toolbar}>
+                        <Typography variant="h4" component="h2">
+                            Segmentation Edit
+                            <div>
+                                <img src="https://placeimg.com/500/800/any"></img>
+                            </div>
+                        </Typography>
+                    </Toolbar>
+                    <div>
+                        <Button
+                            type="submit"
+                            className={classes.submit}
+                            color="primary"
+                            variant="contained"
+                            onClick={this.handleSubmitForm}
+                            fullWidth >Next
+                        </Button>
+                    </div>
+                </div>
+            </Container>
+        );
+    }
+};
+
+export default withSnackbar(withRouter(withStyles(styles) (Segmentation)));
