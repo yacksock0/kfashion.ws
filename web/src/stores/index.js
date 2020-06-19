518e74268280274594e191195b2eb9ee028dd64c
ho <chsgh472@aetherit.io>
2020-06-19 20:21:29 +0900

    SignUp 수정중2

--- web/src/stores/index.js
+++ web/src/stores/index.js
@@ -1,7 +1,9 @@
 import { default as AuthStore } from "./AuthStore";
 import {default as CategoryStore} from "./CategoryStore";
+import SignUpStore from "./SignUpStore";
 
 export const stores = {
+    signUpStore: new SignUpStore(),
     authStore:  new AuthStore(),
     categoryStore : new CategoryStore(),
 };
