c74dda0b7b79e79a7db1ad69b9bae3a26512a421
ho <chsgh472@aetherit.io>
2020-07-09 17:09:02 +0900

    이미지 삭제

--- web/src/views/step2/Color.js
+++ web/src/views/step2/Color.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Select from 'react-select';
 import axios from "axios";
 import {inject, observer} from "mobx-react";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import {Button} from "@material-ui/core";
 
 
 
@@ -11,10 +14,13 @@ export default class SelectTest extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            open:false,
             text: 'text',
             colorList: [],
             selectedOption:null,
         }
+        this.handleClickOpen = this.handleClickOpen.bind(this)
+        this.handleClose = this.handleClose.bind(this);
     }
     componentDidMount() {
         axios.get('/api/v1/kfashion/category/item/basic/color')
@@ -37,18 +43,48 @@ export default class SelectTest extends React.Component {
             { selectedOption }
         );
     };
-
+    handleClickOpen() {
+        this.setState({
+            open: true
+        });
+    }
+    handleClose() {
+        this.setState({
+            open: false
+        });
+    }
     render() {
         const { selectedOption } = this.state;
         const colorList= this.state.colorList;
         return (
-            <Select
-                value={selectedOption}
-                onChange={this.handleChange}
-                options={colorList}
-                autoFocus={true}
-                placeholder={'색상을 선택 하세요'}
-            />
+            <div>
+            <Button variant="contained" color="primary" onClick={this.handleClickOpen}>생삭 지정</Button>
+            <Dialog open={this.state.open} onClose={this.handleClose}
+                    maxWidth={"md"}
+                    fullWidth={"100%"}
+                    height={'100%'}
+            >
+            <DialogContent>
+                <div style={{height:'30vh'}}>
+            <div style={{border: '1px solid red', backgroundColor:"red", width: 50, height: 50}}>
+            </div>
+            <div>
+                빨간색(Red)
+            </div>
+                    <div style={{border: '1px solid blue', backgroundColor:"blue", width: 50, height: 50}}>
+                    </div>
+                    <div>
+                        Blue(파란색)
+                    </div>
+                    <div style={{border: '1px solid black', backgroundColor:"black", width: 50, height: 50}}>
+                    </div>
+                    <div>
+                        Black(검정색)
+                    </div>
+                </div>
+            </DialogContent>
+            </Dialog>
+            </div>
         );
     }
 }
