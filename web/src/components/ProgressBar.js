8107cb43c61429001e5e17794d138604d9182fd1
choi <choi@aetherit.io>
2020-07-13 10:41:08 +0900

    서버 세팅 후 바뀐 디비 연동중 로그인 연동중

--- web/src/components/ProgressBar.js
+++ web/src/components/ProgressBar.js
@@ -20,25 +20,21 @@ export class ProgressBar extends Component {
 
         console.log('id', this.props.rowDataId)
 
-        const workCount = axios.post(`/api/v1/kfashion/work/history/progressRate?createdId=${this.props.rowDataId}`)
+        axios.post(`/api/v1/kfashion/work/history/progressRate?createdId=${this.props.rowDataId}`)
             .then(response => {
-                const workLoadList = response.data.selectWorkProgressRate;
+                const selectWorkProgressRate = response.data.selectWorkProgressRate;
+                const total = selectWorkProgressRate.totalWork;
+                const complete = selectWorkProgressRate.finishWork;
+
                 this.setState({
-                    selectWorkProgressRate: workLoadList(workLoad => {
-                        this.totalWork=workLoad.totalWork;
-                        this.finishWork=workLoad.finishWork;
-                        console.log(workLoad.totalWork);
-                        console.log(workLoad.finishWork);
-                    })
-                })
-                console.log(response.data)
+                    total: selectWorkProgressRate.totalWork,
+                    complete: selectWorkProgressRate.finishWork,
+                    percent : (complete / total) *100
+                });
             })
             .catch(error => {
                 console.log(error)
             })
-
-        console.log(this.totalWork);
-        console.log(this.finishWork);
     }
 
     changeState() {
