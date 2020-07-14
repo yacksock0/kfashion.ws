7b91a5f528c7d05b95d6f3158bdd9cfd2876d17c
ho <chsgh472@aetherit.io>
2020-07-14 09:58:49 +0900

    로고수정

--- web/src/views/finalCheckList.js
+++ web/src/views/finalCheckList.js
@@ -23,6 +23,13 @@ import Search from "@material-ui/icons/Search";
 import ArrowDownward from "@material-ui/icons/ArrowDownward";
 import Remove from "@material-ui/icons/Remove";
 import ViewColumn from "@material-ui/icons/ViewColumn";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 const styles = theme => ({   root: {
         width: "100%",
@@ -127,14 +134,13 @@ class finalCheckList extends React.Component {
             tapIndex: 1,
             createdId: '',
             boundaryList: [],
+            imgData:'',
             count: 0,
             data: [],
             columns: [
                 {title: '번호', field: 'workNo',type: 'button', filterPlaceholder: 'GroupNo filter', tooltip: 'workNo로 정렬'},
-                {title: '사진', field: 'fileName',type: 'Image', render : rowData => <img src={rowData.fileName} style={{width: 50, height:50,}}/> },
                 {title: '이름', field: 'workName',type: 'button', filterPlaceholder: 'GroupNo filter',},
                 {title: '등록자', field: 'createdId', type: 'text', initialEditValue: 'test', tooltip: 'This is tooltip text'},
-                {title: '등록일 ', field: 'createdDatetime', type: 'date'},
             ],
         }
     }
@@ -157,6 +163,11 @@ class finalCheckList extends React.Component {
         })
         this.props.professionalLabelStore.changeNewProfessionalLabelWorkNo(this.props.imageStore.isWorkNo);
     }
+    handleClick=(workNo, imgData)=>{
+        this.setState({
+            imgData:imgData,
+        })
+    }
     handleClickReturn=()=>{
 
     }
@@ -168,8 +179,71 @@ class finalCheckList extends React.Component {
                 <div className={classes.appBarSpacer} />
                 <div className={classes.mainContent}>
                     <Grid container>
-                        <Grid item xs={10} style={{margin:"auto"}}>
-                            <MaterialTable style={{height:'100%'}}
+                        <Grid item xs={5}>
+                            <img src={this.state.imgData} style={{display:"inline-block" , width:650, height:'60vh'}}/>
+                        </Grid>
+                        <Grid item xs={3} style={{marginRight:10}}>
+                            <div>
+                                <TableContainer component={Paper} style={{height:'60vh'}}>
+                                    <Table className={classes.table} aria-label="simple table">
+                                        <TableHead style={{backgroundColor:"primary"}}>
+                                            <TableRow>
+                                                <TableCell align="center">항목</TableCell>
+                                                <TableCell align="center">레이블링</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                                <TableRow>
+                                                    <TableCell align="center">스타일</TableCell>
+                                                    <TableCell align="center"></TableCell>
+                                                </TableRow>
+                                            <TableRow>
+                                                <TableCell align="center">카테고리</TableCell>
+                                                <TableCell align="center"></TableCell>
+                                            </TableRow>
+                                            <TableRow>
+                                                <TableCell align="center">컬러</TableCell>
+                                                <TableCell align="center"></TableCell>
+                                            </TableRow>
+                                            <TableRow>
+                                                <TableCell align="center">디테일</TableCell>
+                                                <TableCell align="center"></TableCell>
+                                            </TableRow>
+                                            <TableRow>
+                                                <TableCell align="center">프린트</TableCell>
+                                                <TableCell align="center"></TableCell>
+                                            </TableRow>
+                                            <TableRow>
+                                                <TableCell align="center">소재</TableCell>
+                                                <TableCell align="center"></TableCell>
+                                            </TableRow>
+                                            <TableRow>
+                                                <TableCell align="center">기장</TableCell>
+                                                <TableCell align="center"></TableCell>
+                                            </TableRow>
+                                            <TableRow>
+                                                <TableCell align="center">소매기장</TableCell>
+                                                <TableCell align="center"></TableCell>
+                                            </TableRow>
+                                            <TableRow>
+                                                <TableCell align="center">넥라인</TableCell>
+                                                <TableCell align="center"></TableCell>
+                                            </TableRow>
+                                            <TableRow>
+                                                <TableCell align="center">칼라</TableCell>
+                                                <TableCell align="left"></TableCell>
+                                            </TableRow>
+                                            <TableRow>
+                                                <TableCell align="center">핏</TableCell>
+                                                <TableCell align="left"></TableCell>
+                                            </TableRow>
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </div>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <MaterialTable
                                 icons={tableIcons
                                 }
                                 columns={this.state.columns}
@@ -192,7 +266,7 @@ class finalCheckList extends React.Component {
                                     {
                                         icon: CheckIcon,
                                         tooltip: 'Select Image',
-                                        // onClick: (event, rowData) => this.handleClick(rowData.workNo, "/api/v1/kfashion/img/getByteImage?workNo="+rowData.workNo)
+                                        onClick: (event, rowData) => this.handleClick(rowData.workNo, "/api/v1/kfashion/img/getByteImage?workNo="+rowData.workNo)
                                     },
                                     {
                                         icon: Clear,
@@ -204,7 +278,6 @@ class finalCheckList extends React.Component {
                         </Grid>
                     </Grid>
                 </div>
-                <hr></hr>
             </Container>
         );
     }
