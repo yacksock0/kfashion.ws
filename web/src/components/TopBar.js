64aa83afbde2ee22a112d49f5c616683a9c2f327
jang6845 <jangseongyeol@aetherit.io>
2020-07-22 12:52:53 +0900

    업로드이미지 수정

--- web/src/components/TopBar.js
+++ web/src/components/TopBar.js
@@ -14,6 +14,7 @@ const useStyles = makeStyles((theme) => ({
         [theme.breakpoints.up('xl')]: {
             width: `calc(100% - ${theme.drawerWidth}px)`,
             marginLeft: theme.drawerWidth,
+            backgroundColor : '#000000',
         },
     },
     menuButton: {
