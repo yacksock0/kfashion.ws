b87fe002f9a8f1c4e35f74b07aca56a6d79f3f72
ho <chsgh472@aetherit.io>
2020-07-22 10:30:57 +0900

    이전작업 이미지 짤림 수정

--- web/src/views/admin/WorkDetail.js
+++ web/src/views/admin/WorkDetail.js
@@ -114,6 +114,8 @@ class WorkDetail extends React.Component {
         return (
             <div>
                 <Button variant="contained" color="primary" onClick={this.handleClickOpen}>작업지정</Button>
+                &nbsp;&nbsp;
+                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>작업취소</Button>
                 <Dialog open={this.state.open} onClose={this.handleClose}
                         maxWidth={"xs"}
                         fullWidth={"100%"}
