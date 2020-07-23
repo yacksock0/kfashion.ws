8bc9469f5b6ff9f85e1dcb2b4ece5b39d293d576
jang6845 <jangseongyeol@aetherit.io>
2020-07-23 18:39:06 +0900

    고등학생 리턴 코멘트 작업

--- src/main/java/io/aetherit/kfashion/ws/controller/KfashionCommentController.java
+++ src/main/java/io/aetherit/kfashion/ws/controller/KfashionCommentController.java
@@ -48,11 +48,7 @@ public class KfashionCommentController {
         kfashionComment.setReceiveId(kfashionWorkHistoryService.selectReceiveId(kfashionComment.getWorkNo()));
         if(kfashionComment.getWorkStep1() == 3) {
             if(kfashionComment.getWorkType1() == 1) {
-                Map<String,Object> selectMap = new HashMap<>();
-                selectMap.put("workNo",kfashionComment.getWorkNo());
-                selectMap.put("workStep",kfashionComment.getWorkStep1());
-                int commentNo =kfashionCommentService.selectCommentNo(selectMap);
-                kfashionComment.setCommentNo(commentNo);
+                kfashionComment.setCommentNo(1);
                 kfashionComment.setWorkStep(3);
                 kfashionComment.setWorkType(1);
                 kfashionCommentService.insertHighPolyComment(kfashionComment);
@@ -117,11 +113,7 @@ public class KfashionCommentController {
             }
         }
         if(kfashionComment.getWorkStep2() == 4) {
-            Map<String,Object> selectMap = new HashMap<>();
-            selectMap.put("workNo",kfashionComment.getWorkNo());
-            selectMap.put("workStep",kfashionComment.getWorkStep1());
-            int commentNo =kfashionCommentService.selectCommentNo(selectMap);
-            kfashionComment.setCommentNo(commentNo);
+            kfashionComment.setCommentNo(1);
             kfashionComment.setWorkStep(4);
             kfashionCommentService.insertHighLabelComment(kfashionComment);
         }
