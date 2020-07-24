558df66ef1a5b391aaa83ede68c708f2915c3df2
ho <chsgh472@aetherit.io>
2020-07-24 21:00:20 +0900

    검수기능 추가중

--- web/src/views/step2/BasicImageList.js
+++ web/src/views/step2/BasicImageList.js
@@ -1,46 +1,7 @@
 
-import React, {forwardRef} from "react";
+import React from "react";
 import MaterialTable from 'material-table';
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
-import axios from "axios";
 import {inject, observer} from "mobx-react";
-import CheckIcon from '@material-ui/icons/Check';
-import {toJS} from "mobx";
-import ErrorIcon from "@material-ui/icons/Error";
-
-const tableIcons = {
-    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-};
 
 @inject('fileUploadStore','authStore','imageStore','polygonStore','basicCategoryStore','checkHighLabelStore')
 @observer
@@ -87,11 +48,9 @@ class BasicImageList extends React.Component {
         }
     }
     render() {
-        const {basicLabelList} = this.state;
         const polyNo = this.props.polygonStore.tabIndex1-1;
         return (
             <MaterialTable
-                icons={tableIcons}
                 columns={this.state.columns}
                 data={!!this.props.polygonStore.polygonList ?
                     this.props.polygonStore.polygonList.map((item) => {
@@ -114,25 +73,23 @@ class BasicImageList extends React.Component {
                         textAlign:'center',
                     },
                     cellStyle: {
-                        textAlign: 'center'
+                        textAlign: 'center',
                     },
                 }}
                 actions={[
                     {
-                        icon: CheckIcon,
+                        icon: 'check',
                         tooltip: 'Select Image',
                         onClick: (event, rowData) => this.handleClick(rowData.workNo, "/api/v1/kfashion/img/getByteImage?workNo="+rowData.workNo,polyNo, rowData.comment)
                     },
                     rowData => ({
-                        icon: ErrorIcon,
+                        icon: 'error',
+                        iconProps: {
+                            style: {color: 'red'}
+                        },
                         tooltip: <h1 style={{lineHeight:1}}>{rowData.comment}</h1>,
                         hidden: rowData.comment == null,
                     })
-                    // {
-                    //     icon: Clear,
-                    //     tooltip: 'return',
-                    //     onClick: (event, rowData) => this.handleClickReturn()
-                    // }
                 ]}
             />
         );
