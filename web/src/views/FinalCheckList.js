e18cba8534447a6e52fb0cb428712d0e05b584dc
jang6845 <jangseongyeol@aetherit.io>
2020-07-27 17:13:41 +0900

    검수 완료 작업

--- web/src/views/FinalCheckList.js
+++ web/src/views/FinalCheckList.js
@@ -218,7 +218,7 @@ class FinalCheckList extends React.Component {
             this.props.imageStore.ProfessionalCompleteUp(workNo, createdId);
             this.setState({
                 open: false,
-                tabIndex1: 1,
+                tabIndex1 : 1,
             })
         }
     }
